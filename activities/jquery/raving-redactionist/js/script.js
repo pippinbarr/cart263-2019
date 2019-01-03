bc14f04897fa166e01f3dabec71cb98cd816f048
Pippin Barr <pippin.barr@gmail.com>
2019-01-03 15:15:10 -0500

    Commented raving redactionist activity

--- activities/jquery/raving-redactionist/js/script.js
+++ activities/jquery/raving-redactionist/js/script.js
@@ -5,31 +5,53 @@
 Raving Redactionist
 Pippin Barr
 
-Keep the document redacted!
+You are redacting a document, but it keeps coming unredacted!
+Click the secret information to hide it, don't let all the
+secrets become revealed!
 
 ******************/
 
+// A place to store the jQuery selection of all spans
 let $spans;
 
+// When the document is loaded we call the setup function
 $(document).ready(setup);
 
-function setup () {
+// setup()
+//
+// Sets the click handler and starts the time loop
+function setup() {
+  // Save the selection of all spans (since we do stuff to them multiple times)
   $spans = $('span');
-
+  // Set a click handler on the spans (so we know when they're clicked)
   $spans.on('click',spanClicked);
-
+  // Set an interval of 500 milliseconds to update the state of the page
   setInterval(update,500);
 };
 
+// spanClicked()
+//
+// When a span is clicked we remove its revealed class and add the redacted class
+// thus blacking it out
 function spanClicked() {
   $(this).removeClass('revealed');
   $(this).addClass('redacted');
 }
 
+// update()
+//
+// Update is called every 500 milliseconds and it updates all the spans on the page
+// using jQuery's each() function which calls the specified function on _each_ of the
+// elements in the selection
 function update() {
   $spans.each(updateSpan);
 }
 
+// updateSpan()
+//
+// With a probability of 10% it unblanks the current span by removing the
+// redacted class and adding the revealed class. Because this function is called
+// by each(), "this" refers to the current element that each has selected.
 function updateSpan() {
   let r = Math.random();
   if (r < 0.1) {
@@ -38,11 +60,11 @@ function updateSpan() {
   }
 }
 
-// Version using anonymous functions:
+// A version using anonymous functions:
 
 // $(document).ready(function () {
 //   $spans = $('span');
-// 
+//
 //   $spans.on('click',function () {
 //     $(this).removeClass('revealed');
 //     $(this).addClass('redacted');
