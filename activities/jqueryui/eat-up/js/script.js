701a265573443baa0b02f82fe404173f3d2a62b4
Pippin Barr <pippin.barr@gmail.com>
2019-01-14 13:34:10 -0500

    Commented Eat Up and Endless Dialogs

--- activities/jqueryui/eat-up/js/script.js
+++ activities/jqueryui/eat-up/js/script.js
@@ -14,43 +14,73 @@ Chewing: https://freesound.org/people/InspectorJ/sounds/412068/
 
 ******************/
 
+// Sound effects for the experience
 let buzzSFX = new Audio("assets/sounds/buzz.mp3");
 let crunchSFX = new Audio("assets/sounds/crunch.wav");
 
+// Variable to hold our two key elements
 let $mouth;
 let $fly;
 
 $(document).ready(setup);
 
 function setup() {
-  $mouth = $('<img src="assets/images/mouth-open.png">');
+  // Get the mouth element from the page
+  $mouth = $('#mouth');
+  // Make it droppable
   $mouth.droppable({
+    // The drop option specifies a function to call when a drop is completed
     drop: flyDropped
   });
 
-  $fly = $('<img src="assets/images/fly.png">');
+  // Get the fly element from the page
+  $fly = $('#fly');
+  // Make it draggable
   $fly.draggable();
+
+  // Start up the buzzing of the fly
   buzzSFX.loop = true;
   buzzSFX.play();
-
-  $('body').append($mouth);
-  $('body').append($fly);
 }
 
+// flyDropped(event,ui)
+//
+// Called when a draggable element is dragged over the droppable element (the mouth)
+// In this instance it can only be the fly (it's the only draggable element).
+// The arguments 'event' and 'ui' are automatically passed by jQuery UI and contain
+// helpful information about the event.
 function flyDropped (event,ui) {
-  ui.draggable.remove();
+  // When we drop the fly into the mouth we should remove the fly from the page
+  // ui contains a reference to the draggable element that was just dropped in ui.draggable
+  // .remove() removes the select element from the page
+  ui.draggable.remove(); // $fly.remove() would work here too
+  // We should "close the mouth" by changing its image
+  // .attr() lets us change specific attributes on HTML element by specifying the attribute
+  // and then what we want to set it to - in this case the 'src' attribute to the closed image
   $(this).attr('src','assets/images/mouth-closed.png');
+  // Now the fly is gone we should stop its buzzing
   buzzSFX.pause();
+  // And start the crunching sound effect of chewing
   crunchSFX.play();
+  // Use a setInterval to call the chew() function over and over
   setInterval(chew,250);
 }
 
+// chew()
+//
+// Swaps the mouth image between closed and open and plays the crunching SFX
 function chew () {
+  // We can use .attr() to check the value of an attribute to
+  // In this case we check if the image is the open mouth
   if ($mouth.attr('src') === 'assets/images/mouth-open.png') {
+    // If it is, we set the 'src' attribute to the closed mouth
     $mouth.attr('src','assets/images/mouth-closed.png');
+    // and play the crunching
     crunchSFX.play();
   }
   else {
+    // Otherwise the 'src' attribute must have been the closed mouth
+    // so we swap it for the open mouth
     $mouth.attr('src','assets/images/mouth-open.png');
   }
 }
--- examples/jqueryui/endless-dialogs/js/script.js
+++ examples/jqueryui/endless-dialogs/js/script.js
@@ -15,9 +15,12 @@ You are deluged with mediocre existentialist philosophy about Sisyphus and Camus
 
 ******************/
 
+// Constants to determine how long after dismissing a dialog we should
+// add a new one to the page
 const MIN_DIALOG_DELAY_TIME = 2000;
 const MAX_DIALOG_DELAY_TIME = 20000;
 
+// An array of inane existentialist questions to ask the user
 let questions = [
   "Must we imagine Sisyphus happy?",
   "If Sisyphus is happy does that mean that we're happy too?",
@@ -36,38 +39,70 @@ let questions = [
   "You are in a maze of twisty passages, all alike. Sisyphus is here and offers to let you push his rock uphill. Will you?"
 ];
 
+// Load sound effects for dialogs appearing and being dismissed
 let newDialogSFX = new Audio("assets/sounds/dialog_new.wav");
 let dismissDialogSFX = new Audio("assets/sounds/dialog_dismiss.wav");
 
+// We want to track how much the mouse is moved and when it reaches a maximum
+// create a new dialog, so here are a constant and a variable to track that
 const MAX_MOUSE_MOVES = 20;
 let mouseMoves = 0;
 
 $(document).ready(setup);
 
 function setup() {
+  // Whenever the mouse moves, call the mouseMoved function
   $(document).on('mousemove',mouseMoved);
+  // After one second, add a dialog to the page
   setTimeout(addDialog,1000);
 }
 
+// mouseMoved()
+//
+// Called whenever the mouse it moved on the page. Used here as a way to track
+// user interaction with the page.
 function mouseMoved() {
+  // Increase the number of tracked move
   mouseMoves++;
+  // Check if they exceed the maximum
   if (mouseMoves > MAX_MOUSE_MOVES) {
+    // If so, add a dialog
     addDialog();
+    // And reset the counter
     mouseMoves = 0;
   }
 }
 
+// addDialog()
+//
+// The key function. It adds a stupid dialog to a random position
+// on the screen.
 function addDialog () {
+  // Play the new dialog sound effect
   newDialogSFX.currentTime = 0;
   newDialogSFX.play();
 
+  // Dynamically create a div and store it in a variable. This is the div
+  // we will turn into a dialog box
   let $dialog = $('<div></div>');
+  // Give it a title attribute (display in the title bar of the dialog)
   $dialog.attr('title','Question');
+  // Choose a random question text
   let question = questions[Math.floor(Math.random() * questions.length)];
+  // Add a p tag to the dialog div that contains the question text
   $dialog.append("<p>" + question + "</p>");
+  // Finally, add the div to the page
   $('body').append($dialog);
 
+  // Now we have our div on the page, transform it into a dialog with jQuery UI's
+  // .dialog() method, supplying a number of options to configure it
   $dialog.dialog({
+    // The 'buttons' option lets us specify buttons to appear in the dialog as
+    // the properties of an object. The property name is used as the button text
+    // and the property contains a function that will be called when that button
+    // is clicked. Note how you can have quote marks around a property name (important
+    // if you want to include spaces for instance.)
+    // In this case both buttons just close the dialog
     buttons: {
       "Yes?": function () {
         $(this).dialog('close');
@@ -76,15 +111,23 @@ function addDialog () {
         $(this).dialog('close');
       }
     },
+    // THe 'close' option lets us specify a function to call when the dialog is closed
     close: function () {
+      // Play the dismissal sound, ding!
       dismissDialogSFX.currentTime = 0;
       dismissDialogSFX.play();
+      // Choose a random delay time (in ms)
       let delay = MIN_DIALOG_DELAY_TIME + Math.random() * (MAX_DIALOG_DELAY_TIME - MIN_DIALOG_DELAY_TIME);
+      // Set a timeout and add a new dialog after the delay. Dismiss a dialog, and you just get another one back
       setTimeout(addDialog,delay);
     },
+    // The 'containment' option lets us specify where the dialog can go on the screen. 'body' means it will be
+    // contained within the body tag, and can't be dragged out of it.
     containment: 'body'
   });
 
+  // Finally, use .offset() on the .parent() of the dialog in order to give it a random position on the screen.
+  // Uses .height() and .width() to get the dimensions of elements, including the window.
   $dialog.parent().offset({
     top: Math.random() * ($(window).height() - $dialog.parent().height()),
     left: Math.random() * ($(window).width() - $dialog.parent().width())
